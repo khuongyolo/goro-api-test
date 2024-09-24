cf5478b83e598e92d76674048d626627685243ea
khuongyolo <khuongyolo@gmail.com>
2024-09-25 00:08:19 +0900

    update webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -8,22 +8,28 @@ class WebhookController extends Controller
 {
     public function handleWebhook(Request $request)
     {
-        // Đường dẫn tới thư mục chứa code
-        $repoDir = '/var/www/api.goro.fun';
+        try {
+            // Đường dẫn tới thư mục chứa code
+            $repoDir = '/var/www/api.goro.fun';
 
-        // Xác thực webhook (nếu cần)
-        $secret = 'jdy4758fjgnd5'; // Nếu bạn đã thiết lập secret khi tạo webhook
-        $signature = 'sha1=' . hash_hmac('sha1', $request->getContent(), $secret);
+            // Xác thực webhook (nếu cần)
+            $secret = 'jdy4758fjgnd5'; // Nếu bạn đã thiết lập secret khi tạo webhook
+            $signature = 'sha1=' . hash_hmac('sha1', $request->getContent(), $secret);
 
-        // Kiểm tra chữ ký hợp lệ
-        if (hash_equals($signature, $request->header('X-Hub-Signature'))) {
-            // Nhận webhook thành công, thực hiện git pull
-            shell_exec("cd {$repoDir} && sudo git pull");
+            // Kiểm tra chữ ký hợp lệ
+            if (hash_equals($signature, $request->header('X-Hub-Signature'))) {
+                // Nhận webhook thành công, thực hiện git pull
+                shell_exec("cd {$repoDir} && sudo git pull");
 
-            return response()->json(['status' => 'Success'], 200);
-        } else {
-            // Webhook không hợp lệ
-            return response()->json(['status' => 'Forbidden'], 403);
+                return response()->json(['status' => 'Success'], 200);
+            } else {
+                // Webhook không hợp lệ
+                return response()->json(['status' => 'Forbidden'], 403);
+            }
+        }
+        catch (Exception $e) {
+            // 例外が発生した場合、ログを出力する
+            Log::error(__CLASS__ . ', ' . __FUNCTION__ . ', SYS-WEBHOOK, ' . $e->getMessage());
         }
     }
 }
