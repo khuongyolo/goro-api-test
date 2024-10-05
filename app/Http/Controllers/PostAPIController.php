55d5e63fecc58fc8266eb07ad7f497e1222c1037
khuongyolo <khuongyolo@gmail.com>
2024-10-05 16:09:09 +0900

    update post's update

--- app/Http/Controllers/PostAPIController.php
+++ app/Http/Controllers/PostAPIController.php
@@ -96,7 +96,8 @@ class PostAPIController extends Controller
     function update(PostAPIRequest $request)
     {
         try {
-            $id = Cache::get('post.id', $request->id);
+            // $id = Cache::get('post.id', $request->id);
+            $id = $request->id;
             $post = Post::find($id);
             if (empty($post)) {
                 return response()->json([
